3f761802a412fa45e43096980dee13ae23077049
azure-lio <123126403@qq.com>
2021-02-22 17:48:58 +0800

    Add files via upload

---
+++ installv2.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#Basic enviroment set
+
+set -x
+
+TOP_DIR=$(cd $(dirname "$0") && pwd)
+source $TOP_DIR/common
+GetOSVersion
+
+if [[ "$os_VENDOR" =~ (Ubuntu)  ]]; then
+         echo "OS vendor is Ubutu" $os_RELEASE
+elif [[ "$os_VENDOR" =~ (CentOS) ]]; then
+         echo "OS Vendor is Centos" $os_RELEASE
+else
+         echo "Not support this os vendor"
+         exit 0
+fi
+
+init_lib_common
+
+source $TOP_DIR/functions
+
+
+
+
+function_main()
+{
+    
+    env_set
+    function_chrony
+    function_openstack_pkg
+#First install controller ,then check whether need to install compute
+    if [ $controller -eq 1 ] ; then
+        function_mariadb
+        install_memcache
+        install_rabbitmq
+        install_etcd
+        install_keystone
+        install_glance
+        install_controller_nova
+        install_controller_neutron 
+        install_dashboard
+        echo "=====Install Successfully  within a control node====="
+        echo "Your dashborad address:http://$HOST_IP/dashboard/"
+        echo "Doamin:Default"
+        echo "User name:admin"
+        echo "Password:$PASSWORD"
+    fi
+#IF in allinone mode ,we install control modules before this
+
+    if [ $compute -eq 1 ] ; then
+        install_compute_nova
+        install_compute_neutron
+    fi
+    
+
+}
+
+#call main
+function_main
+
